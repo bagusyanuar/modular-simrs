cc7df7a453bf4243eba8a37e0f5600a789bd5839
bagusyanuar <bagus.yanuar613@gmail.com>
2026-03-19 15:52:23 +0700

    add visualizer

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    visualizer({
+      open: true, // otomatis buka browser setelah build
+      gzipSize: true, // tampilkan ukuran setelah gzip
+      brotliSize: true, // tampilkan ukuran setelah brotli
+      filename: 'dist/stats.html',
+      template: 'treemap',
+    }),
   ],
 });
